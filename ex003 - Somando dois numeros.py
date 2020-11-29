0f5a6b7406fa47dbb7eeb919dd4f9d15e549559d
lucsnguedes <75098996+lucsnguedes@users.noreply.github.com>
2020-11-28 21:51:34 -0300

    Add files via upload

---
+++ ex003 - Somando dois numeros.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+s = n1 + n2
+print('A soma entre \033[31m{}\033[m e \033[31m{}\033[m é igual a \033[32m{}\033[m!'.format(n1, n2, s))
